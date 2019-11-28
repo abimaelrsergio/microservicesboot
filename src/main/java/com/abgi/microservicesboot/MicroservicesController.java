d128b9caf6bd20a3a0960b0643289125a4e25cdc
Abimael <abimael.sergio-ext@viavarejo.com.br>
2019-11-28 05:44:23 -0300

    Atualizacao

--- src/main/java/com/abgi/microservicesboot/MicroservicesController.java
+++ src/main/java/com/abgi/microservicesboot/MicroservicesController.java
@@ -1,8 +1,14 @@
 package com.abgi.microservicesboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MicroservicesController {
 
+	@RequestMapping("/hi")
+	public @ResponseBody String hiThere() {
+		return "Ola mundo!";
+	}
 }
